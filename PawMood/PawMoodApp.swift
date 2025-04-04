dbc2f42940f559956aba2b3d432e50852ed4a565
` <pw1298@nyu.edu>
2025-04-03 21:37:13 -0400

    init SwiftUI project

---
+++ PawMood/PawMoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawMoodApp.swift
+//  PawMood
+//
+//  Created by WPH on 2025/4/3.
+//
+
+import SwiftUI
+
+@main
+struct PawMoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PawMoodTests/PawMoodTests.swift
@@ -0,0 +1,16 @@
+//
+//  PawMoodTests.swift
+//  PawMoodTests
+//
+//  Created by WPH on 2025/4/3.
+//
+
+import Testing
+
+struct PawMoodTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
